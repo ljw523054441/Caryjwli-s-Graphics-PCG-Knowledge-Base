1a64bbeca2d390370d650948f23366ec733e561e
JW Li <47248993+ljw523054441@users.noreply.github.com>
2025-12-06 15:08:50 +0800

    修改网站标题

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "我的笔记",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
