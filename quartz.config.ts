3d2d00f5eda4ab269db2b39135fddf9c7f89548e
JW Li <47248993+ljw523054441@users.noreply.github.com>
2025-12-08 10:58:10 +0800

    修改baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ljw523054441.github.io/Caryjwli-s-Graphics-PCG-Knowledge-Base",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
